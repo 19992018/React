1f570b32ea788dcbd8e0a0da39ab796fc0b8d0f0
19992018 <margretmauno16@gmail.com>
2024-04-18 09:25:55 +0300

    different implementation of alert appearance

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,22 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
-  const [ourClass, setourclass] = useState("");
-  const [wordtoggle, setwordtoggle] = useState("Hide");
-
-  const setOnClick = () => {
-    if (ourClass == "") {
-      setourclass("fade");
-    } else {
-      setourclass("");
-    }
-
-    if (wordtoggle == "show") {
-      setwordtoggle("Hide");
-    } else {
-      setwordtoggle("Show");
-    }
-  };
+  const [visibility, setVisibility] = useState(false);
 
   return (
     <div>
-      <Alert classdefiner={ourClass}>
-        Heyy now you see <strong>Me</strong>
-      </Alert>
+      {visibility && (
+        <Alert onClose={() => setVisibility(false)}>
+          Heyy now you see <strong>Me</strong>
+        </Alert>
+      )}
       {/* <Button onClick={console.log("Heyy")}>Click Me</Button>
       <Button onClick={console.log("Heyy")}>And Mee</Button> */}
-      <Button color="btn-secondary" onClick={setOnClick}>
-        {wordtoggle}
+      <Button color="btn-secondary" onClick={() => setVisibility(true)}>
+        See Alert
       </Button>
       <Button color={"btn-success"} onClick={() => console.log("Heyy")}>
-        And Mee
+        Ignore me
       </Button>
     </div>
   );
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,23 +2,21 @@ import { ReactNode } from "react";
 
 interface AlertProp {
   children: ReactNode;
-  classdefiner: string;
+  onClose: () => void;
 }
 
-const Alert = ({ children, classdefiner }: AlertProp) => {
+const Alert = ({ children, onClose }: AlertProp) => {
   // return <div className="alert alert-success">{children}</div>;
   return (
     <div>
-      <div
-        className={"alert alert-success alert-dismissible " + classdefiner}
-        role="alert"
-      >
+      <div className={"alert alert-success alert-dismissible"} role="alert">
         {children}
         <button
           type="button"
           className="btn-close"
           data-bs-dismiss="alert"
           aria-label="Close"
+          onClick={onClose}
         ></button>
       </div>
       ;
